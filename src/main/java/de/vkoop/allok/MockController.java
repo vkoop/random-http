5d6aff861abeb5d1ba2d2b904cb00ed1b3cb7b8a
Viktor Koop <viktor.koop@googlemail.com>
2021-04-03 10:58:53 +0200

    initi

---
+++ src/main/java/de/vkoop/allok/MockController.java
@@ -0,0 +1,40 @@
+package de.vkoop.allok;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Random;
+
+@Controller
+public class MockController {
+
+    private static final Random random = new Random();
+
+    @Value("${expectation}")
+    private long expectation = 500;
+
+    @Value("${derivation}")
+    private double derivation = 100;
+
+    @ResponseBody()
+    @RequestMapping(value = "/**", produces = "text/html")
+    public Mono<String> answerAll() {
+        final long delay = getDelay();
+        return Mono.delay(Duration.ofMillis(delay))
+                .map(x ->
+                        "<html><head></head><body>ok</body></html>");
+    }
+
+    private long getDelay() {
+        var gaussian = random.nextGaussian();
+
+        var delay = (expectation + derivation * gaussian);
+        delay = Math.max(delay, 0);
+        return (long) delay;
+    }
+
+}
